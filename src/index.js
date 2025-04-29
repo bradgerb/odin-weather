d9ed72bb20edc0c8122e0a8ff42527b3556f731d
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-04-29 15:49:20 -0400

    Set up form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import "./styles.css";
 
+const weatherDataForm = document.getElementById('weatherDataForm');
+
+async function getWeather(location) {
+    try{
+        console.log(location);
+    } catch(error){
+        console.log('ERROR');
+    };
+};
+
+weatherDataForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const formData = new FormData(weatherDataForm);
+    getWeather(formData.get('location'));
+});
