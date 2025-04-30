88a891dc2478615156396cd9cf8ed163491e8282
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-04-30 18:00:36 -0400

    Complete DOM function

--- src/DOM.js
+++ src/DOM.js
@@ -2,14 +2,31 @@ const weatherDisplay = document.querySelector('.weatherDisplay');
 
 const updateWeatherDisplay = (processedData)=> {
     clearWeatherDisplay();
+    addWeatherHeaders();
 
     console.table(processedData.weatherInfo);
     console.log(processedData.currentConditions);
 
     for (let i = 0; i < processedData.weatherInfo.length; i++){
-        const day = document.createElement('div');
-        day.textContent = processedData.weatherInfo[i].date;
-        weatherDisplay.appendChild(day);
+        const date = document.createElement('div');
+        date.classList.add('day');
+        date.textContent = processedData.weatherInfo[i].date;
+        weatherDisplay.appendChild(date);
+
+        const maxTemp = document.createElement('div');
+        maxTemp.classList.add('day');
+        maxTemp.textContent = `${processedData.weatherInfo[i].maxTemp} \u00B0F`;
+        weatherDisplay.appendChild(maxTemp);
+
+        const minTemp = document.createElement('div');
+        minTemp.classList.add('day');
+        minTemp.textContent = `${processedData.weatherInfo[i].minTemp} \u00B0F`;
+        weatherDisplay.appendChild(minTemp);
+
+        const precipitationChance = document.createElement('div');
+        precipitationChance.classList.add('day');
+        precipitationChance.textContent = `${processedData.weatherInfo[i].precipitationChance} %`;
+        weatherDisplay.appendChild(precipitationChance);
     }
 };
 
@@ -18,5 +35,29 @@ const clearWeatherDisplay = ()=>{
         weatherDisplay.removeChild(weatherDisplay.firstChild);
     }
 }
+
+const addWeatherHeaders = ()=>{
+    const dateHeader = document.createElement('div');
+    dateHeader.classList.add('weatherHeader');
+    dateHeader.textContent = 'Date';
+    weatherDisplay.appendChild(dateHeader);
+
+    const maxTempHeader = document.createElement('div');
+    maxTempHeader.classList.add('weatherHeader');
+    maxTempHeader.textContent = 'Maximum Temperature';
+    weatherDisplay.appendChild(maxTempHeader);
+
+    const minTempHeader = document.createElement('div');
+    minTempHeader.classList.add('weatherHeader');
+    minTempHeader.textContent = 'Minimum Temperature';
+    weatherDisplay.appendChild(minTempHeader);
+
+    const precipitationChance = document.createElement('div');
+    precipitationChance.classList.add('weatherHeader');
+    precipitationChance.textContent = 'Precipitation Chance';
+    weatherDisplay.appendChild(precipitationChance);
+}
+
+
 
 export { updateWeatherDisplay }
