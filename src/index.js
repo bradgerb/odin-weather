f8fd5360338073adaebb20771f13b4f54637b1af
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-04-30 18:07:28 -0400

    Allow city input

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const weatherDataForm = document.getElementById('weatherDataForm');
 
 async function getWeather(location) {
     try{
-        const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=HQEVSF9RKDLNF6WJDQU6A82EN', {mode: 'cors'});
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=HQEVSF9RKDLNF6WJDQU6A82EN`, {mode: 'cors'});
         const weatherData = await response.json();
         const processedData = processData(weatherData);
         updateWeatherDisplay(processedData);
@@ -20,3 +20,5 @@ weatherDataForm.addEventListener('submit', (e)=> {
     const formData = new FormData(weatherDataForm);
     getWeather(formData.get('location'));
 });
+
+// getWeather('london');
